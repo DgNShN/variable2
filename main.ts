c81b3567bb419fa1e7a4909da1c6f1bef59bdba7
DGNSHN <33982400+DgNShN@users.noreply.github.com>
2023-02-05 03:52:58 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,33 @@
+let Time = 0
+input.onButtonPressed(Button.A, function () {
+    Time += 300
+})
+input.onButtonPressed(Button.B, function () {
+    Time += -300
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(Time)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.pause(Time)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.pause(Time)
+})
